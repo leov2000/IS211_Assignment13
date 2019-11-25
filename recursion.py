ee0735067203ba51422d31e220bd3c766d693511
leo <leovergarah@gmail.com>
2019-11-24 19:57:03 -0500

    (Added) doc string and fixed syntax

--- recursion.py
+++ recursion.py
@@ -1,4 +1,13 @@
 def fibonacci(num):
+    """
+    A recursive function that retrieves a number within a fib sequence.
+
+    Parameters:
+        num(int)
+    
+    Returns:
+        an int.
+    """
     if num < 1:
         return num
     
@@ -8,39 +17,38 @@ def fibonacci(num):
     return fibonacci(num - 1) + fibonacci(num - 2)
 
 def gcd(a, b):
-  """
-  A recursive function that computes the greatest common divisor.
+    """
+    A recursive function that computes the greatest common divisor.
 
-  Parameters:
-    a(int)
-    b(int)
-  
-  Returns:
-    an int
-  """
-    if not b :
-        return a
+    Parameters:
+        a(int)
+        b(int)
     
+    Returns: an int
+    """
+    if not b:
+        return a
+
     return gcd(b, a % b)
  
 def compareTo(s1, s2):
-  """
-  A recursive function that compares one string to another.
+    """
+    A recursive function that compares one string to another.
 
-  Parameters:
-    s1(string)
-    s2(string)
+    Parameters:
+        s1(string)
+        s2(string)
 
-  Returns:
-    0 if they equal
-    1 if s1 is greater
-   -1 if s2 is greater
-  """
-  if(s1 == s2):
-    return 0
-  elif(s1 > s2):
-    return 1
-  elif(s1 < s2):
-    return -1
-  else:
-    return compareTo(s1[1:], s2[1:])
+    Returns:
+        0 if they equal
+        1 if s1 is greater
+        -1 if s2 is greater
+    """
+    if s1 == s2:
+        return 0
+    elif s1 > s2:
+        return 1
+    elif s1 < s2:
+        return -1
+    else:
+        return compareTo(s1[1:], s2[1:])
